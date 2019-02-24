935b7ff5f2fa09898cc09d7c33be88808ea44fc5
DimaKuptsov <dkuptsov.ua@gmail.com>
2019-02-24 09:12:35 +0200

    add seeds for db

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,6 +7,8 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->email,
         'name' => $faker->unique()->name(),
         'password' => $faker->unique()->numberBetween(1000,2000),
-        'role_id' => rand(0,1)
+        'role_id' => rand(0,1),
+        'image' => $faker->imageUrl($width = 50, $height = 50, 'cats'),
+        'image_public_id' => $faker->text(20)
     ];
 });
