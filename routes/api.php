84651cdfeb56e4b63445ff12b27ffcef0d557b40
nana <nastyaa1212@gmail.com>
2019-02-24 07:02:46 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::prefix('admin')->group(function () {
     Route::delete('/category', 'CategoryController@deleteCategory')->name('deleteCategory')->middleware(['my.basic', 'isAdmin']);
 
 
-    Route::post('/news/create', 'ArticleController@createArticle')->name('createArticle')
+    Route::post('/news/create', 'ArticleController@createArticle')->name('createArticle');
     Route::put('/news/update', 'ArticleController@updateArticle')->name('updateArticle');
     Route::delete('/news/delete', 'ArticleController@deleteArticle')->name('deleteArticle')->middleware(['my.basic', 'isAdmin']);
     Route::put('/news/update/active-status', 'ArticleController@changeIsActiveStatus')->name('changeIsActive')->middleware(['my.basic', 'isAdmin']);
